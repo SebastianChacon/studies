b2ea65b97b859ac040b6b1e26988318f5f51f199
Sebastian Chacon <chacontsebastian@gmail.com>
2026-04-27 17:47:43 -0400

    exrcise 8

--- python/python_course/app.py
+++ python/python_course/app.py
@@ -1,10 +1,15 @@
-# Assignment 7
+# Assignment: calculate weight
 
-name = input("what is your name? ")
+weight = input("what is your weight? ")
+weight_type = input("L(lb) or K(kg)? ")
 
-if len(name) <= 3:
-    print("name be at least 3 characters")
-elif len(name) >= 50:
-    print("name be a maximum 50 characters")
-else:
-    print("Name looks fine!!")
+if weight_type.upper() == "L":
+    weight = float(weight) * 0.45
+    weight_type = "KG"
+elif weight_type.upper() == "K":
+    weight_type = "LB"
+    weight = float(weight) * 2.2     
+# else:
+#     print("you son of bitch give me L or K")
+
+print(f'your weight is {weight} {weight_type}')
