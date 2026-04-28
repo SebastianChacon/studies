aa527fda456cc369507f9f224b7d074fce4f2e49
Sebastian Chacon <chacontsebastian@gmail.com>
2026-04-28 12:38:59 -0400

    exrcise 11

--- python/python_course/app.py
+++ python/python_course/app.py
@@ -1,18 +1,7 @@
+# Assignment: calculate all prices of the shop
 
-# Assignment: Car game
+shop = [10, 20, 30]
 
-while 0 < 1:
-    command = input(">")
-
-    if command.upper() == "HELP":
-        print("""
-            start - to start the car
-            stop - to stop the car
-            quit - to exit """)
-    elif command.upper() == "START":
-        print("Start the car")
-    elif command.upper() == "STOP":
-        print("Stop the car")
-    elif command.upper() == "QUIT":
-        print("Exit the game")
-        break
+for item in shop:
+    item += item
+    print(item)
