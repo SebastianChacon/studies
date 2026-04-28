9dff614b36805109503a9961e7281b2c467023ae
Sebastian Chacon <chacontsebastian@gmail.com>
2026-04-28 13:11:12 -0400

    exrcise 13

--- python/python_course/app.py
+++ python/python_course/app.py
@@ -1,11 +1,11 @@
-# Assignment FOR
+# Assignment: the largest number of the list
 
-list = [ 5, 2, 5, 2, 2]
+list_number = [ 2, 34, 53, 92, 200, 34, 543, 12]
+largest_number = 0
 
-for x in list:
-    output = ''
-    for y in range(x):
-        output += 'x'
+for num in list_number:
+    if num > largest_number:
+        largest_number = num
 
-    print(output)
+print(f"largest_num: {largest_number} - {list_number.index(largest_number)} - {list_number[6]}")
     
