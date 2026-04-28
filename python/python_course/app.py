dabca88bf0c78137f1fb0e286e78c8d364ce98f9
Sebastian Chacon <chacontsebastian@gmail.com>
2026-04-28 11:16:41 -0400

    exrcise 9

--- python/python_course/app.py
+++ python/python_course/app.py
@@ -1,15 +1,14 @@
-# Assignment: calculate weight
+# Assignment: create a guess number
 
-weight = input("what is your weight? ")
-weight_type = input("L(lb) or K(kg)? ")
+tries = 1
 
-if weight_type.upper() == "L":
-    weight = float(weight) * 0.45
-    weight_type = "KG"
-elif weight_type.upper() == "K":
-    weight_type = "LB"
-    weight = float(weight) * 2.2     
-# else:
-#     print("you son of bitch give me L or K")
+while tries <= 3:
+    tries += 1
+    num = int(input("guess number: "))
+    if num == 4:
+        print("you win!!")
+        break
+    
 
-print(f'your weight is {weight} {weight_type}')
+else: 
+    print("You failed!!")
