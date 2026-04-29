d4e8885723eab7480c24f76c9f6d9012f0e58976
Sebastian Chacon <chacontsebastian@gmail.com>
2026-04-28 20:05:33 -0400

    exrcise 14

--- python/python_course/app.py
+++ python/python_course/app.py
@@ -1,11 +1,10 @@
-# Assignment: the largest number of the list
+# Assignment: remove duplicate values of the list
 
-list_number = [ 2, 34, 53, 92, 200, 34, 543, 12]
-largest_number = 0
+duplicate = [ 2, 3, 4, 4, 2, 5, 3, 7, 2]
+unique = []
 
-for num in list_number:
-    if num > largest_number:
-        largest_number = num
+for item in duplicate:
+    if item not in unique:
+        unique.append(item)
 
-print(f"largest_num: {largest_number} - {list_number.index(largest_number)} - {list_number[6]}")
-    
+print(unique)
